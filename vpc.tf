395d5f1949ecbe1b2b1963917a59445c6b61120b
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-23 06:28:06 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "main" {             # giving an internal reference name as main
     cidr_block = var.vpc_cidr
-    instance_tenancy = "default"
+    instance_tenancy = "default"        #or dedicated
     enable_dns_hostnames = true
 
     tags = merge(
